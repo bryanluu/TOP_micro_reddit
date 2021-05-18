aa2868ca6c058842bb945401d42a0ab55b6df786
Bryan Luu <loc.bryan.luu@gmail.com>
2021-05-18 16:18:33 -0500

    Added User skeleton which validates

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 4..25 }, uniqueness: true
+  validates :password, presence: true,
+                       length: { in: 8..100 },
+                       format: { with: /\A[a-zA-Z0-9_]+\z/,
+                                 message: 'must be alphanumeric (with optional underscores)' }
+end
