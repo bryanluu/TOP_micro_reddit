a77ef3f12cd7b6e558307e679e0b66047ab7d8fc
Bryan Luu <loc.bryan.luu@gmail.com>
2021-05-18 16:33:35 -0500

    Add Post model and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   validates :username, presence: true, length: { in: 4..25 }, uniqueness: true
   validates :password, presence: true,
                        length: { in: 8..100 },
